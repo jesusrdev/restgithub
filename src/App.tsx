718f5158c173e12856a51083882e9c02c5700f3c
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-09-20 17:05:37 -0500

    fetching github api with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,56 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+type Repository = {
+  id: number;
+  full_name: string;
+  html_url: string;
+};
 
 function App() {
+  const [keyword, setKeyword] = useState("");
+  const [repodata, setRepodata] = useState<Repository[]>([]);
+
+  const handleClick = async () => {
+    try {
+      const response = await axios.get<{ items: Repository[] }>(
+        `https://api.github.com/search/repositories?q=${keyword}`
+      );
+
+      setRepodata(response.data.items);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
+      <input
+        type="text"
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
+      <button onClick={handleClick}>Fetch</button>
+
+      {repodata.length === 0 ? (
+        <p>No results found</p>
+      ) : (
+        <table>
+          <tbody>
+            {repodata.map((repo) => (
+              <tr key={repo.id}>
+                <td>{repo.full_name}</td>
+                <td>
+                  <a href={repo.html_url}>{repo.html_url}</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
